123c8e3668de703af8277906702c0dfad340694a
Shaman <pieas.asif4@gmail.com>
2024-10-26 05:47:37 +0600

    add: webhost with cdk v1

--- bin/static_web_host_cdk.ts
+++ bin/static_web_host_cdk.ts
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { StaticWebHostCdkStack } from '../lib/static_web_host_cdk-stack';
+import "source-map-support/register";
+import * as dotenv from "dotenv";
+import * as cdk from "aws-cdk-lib";
+import { StaticWebHostCdkStack } from "../lib/static_web_host_cdk-stack";
+
+dotenv.config();
+
+const s3BucketName = process.env.S3_BUCKET_NAME;
+if (s3BucketName == undefined) {
+  console.log("Environment 'S3_BUCKET_NAME' cannot be empty");
+  process.exit();
+}
 
 const app = new cdk.App();
-new StaticWebHostCdkStack(app, 'StaticWebHostCdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new StaticWebHostCdkStack(app, "StaticWebHostCdkStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+  s3BucketName: s3BucketName,
 });
--- lib/static_web_host_cdk-stack.ts
+++ lib/static_web_host_cdk-stack.ts
@@ -1,16 +1,58 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3Deployment from "aws-cdk-lib/aws-s3-deployment";
+import * as cloudFront from "aws-cdk-lib/aws-cloudfront";
+import * as cloudFrontOrigins from "aws-cdk-lib/aws-cloudfront-origins";
+
+import * as path from "path";
+
+interface StaticWebHostCdkStackProps extends cdk.StackProps {
+  s3BucketName: string;
+}
 
 export class StaticWebHostCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: StaticWebHostCdkStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, props.s3BucketName, {
+      accessControl: s3.BucketAccessControl.PRIVATE,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'StaticWebHostCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3Deployment.BucketDeployment(
+      this,
+      `${props.s3BucketName}+Deployment`,
+      {
+        destinationBucket: bucket,
+        sources: [s3Deployment.Source.asset(path.resolve(__dirname, "./dist"))],
+      }
+    );
+
+    const originAccessIdentity = new cloudFront.OriginAccessIdentity(
+      this,
+      `${props.s3BucketName}+AccessIdentity`
+    );
+    bucket.grantRead(originAccessIdentity);
+
+    const bucketOrigin =
+      cloudFrontOrigins.S3BucketOrigin.withOriginAccessIdentity(bucket, {
+        originAccessIdentity: originAccessIdentity,
+      });
+
+    const distribution = new cloudFront.Distribution(
+      this,
+      `${props.s3BucketName}+Distribution`,
+      {
+        defaultRootObject: "index.html",
+        defaultBehavior: {
+          origin: bucketOrigin,
+        },
+      }
+    );
+
+    new cdk.CfnOutput(this, "WebsiteURL", {
+      value: distribution.distributionDomainName,
+    });
   }
 }
